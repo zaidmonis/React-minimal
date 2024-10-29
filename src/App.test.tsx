f2d5a986f392a8047eb28b036c2531c0d8687b30
Zaid <monis.z.786@gmail.com>
2024-10-29 13:45:48 +0530

    add simple api call with axios

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders loading state initially', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/loading/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('test weather data is being rendered properly', async () => {
+  const mockWeatherData = [
+    {
+      date: '2024-10-01',
+      temperatureC: 37,
+      summary: "Scorching",
+      temperatureF: 98
+    },
+    {
+      date: '2024-10-01',
+      temperatureC: -40,
+      summary: "Chilling",
+      temperatureF: -40
+    }
+  ];
+
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(mockWeatherData),
+    })) as jest.Mock
+
+    render(<App />)
+
+    await waitFor(() => expect(screen.queryByText(/loading/i)).toBeNull());
+
+    expect(screen.getByText(/Scorching/i)).toBeInTheDocument();
+    expect(screen.getByText(/Chilling/i)).toBeInTheDocument();
+    const forecast  = screen.getAllByText(/Temperature:/i)
+    expect(forecast.length).toBe(mockWeatherData.length)
+    expect(screen.getByText(/Weather forecast/i)).toBeInTheDocument();
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
 function App() {
+  const [weatherData, setWeatherData] = useState<WeatherForecast[]>([]);
+  const [loading, setLoading] =useState(true);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const response = await fetch('http://localhost:5220/weatherforecast');
+        const data = await response.json();
+        setWeatherData(data);
+      } catch(error) {
+        console.error('Error fetching data: ', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchWeatherData();
+  }, []);
+
+  if(loading) {
+    return <div> Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather Forecast</h1>
+      <ul>
+        {weatherData.map((weather, index) => (
+          <li key={index}>
+            <strong>Date:</strong> {new Date(weather.date).toLocaleDateString()} <br />
+            <strong>Temperature:</strong> {weather.temperatureC} °C / {weather.temperatureF} °F <br />
+            <strong>Summary:</strong> {weather.summary}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
